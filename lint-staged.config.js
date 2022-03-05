c8a6428fe7197d73b999d1cca576a19ef64fbef5
Richard Cyrus <richard.cyrus@rcyrus.com>
2022-03-05 13:12:57 -0500

    Refactor lint-staged script.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,14 +2,28 @@ const micromatch = require("micromatch");
 
 module.exports = {
   "*": (allFiles) => {
+    const commands = [];
+
+    // Type check TypeScript files
+    const typeScriptFiles = micromatch(allFiles, ["**/*.ts?(x)"]);
+    if (typeScriptFiles.length > 0) {
+      commands.push("tsc -p tsconfig.json --noEmit");
+    }
+
     // Lint and format TypeScript and JavaScript files.
     const codeFiles = micromatch(allFiles, ["**/*.[tj]s?(x)"]);
+    if (codeFiles.length > 0) {
+      commands.push(
+        `eslint --cache --fix --ignore-path .gitignore ${codeFiles.join(" ")}`
+      );
+    }
+
     // Format Markdown and JSON files.
     const miscFiles = micromatch(allFiles, ["**/*.md", "**/*.json"]);
-    return [
-      "tsc -p tsconfig.json --noEmit",
-      `eslint --cache --fix --ignore-path .gitignore ${codeFiles.join(" ")}`,
-      `prettier --write ${miscFiles.join(" ")}`,
-    ];
+    if (miscFiles.length > 0) {
+      commands.push(`prettier --write ${miscFiles.join(" ")}`);
+    }
+
+    return commands;
   },
 };
