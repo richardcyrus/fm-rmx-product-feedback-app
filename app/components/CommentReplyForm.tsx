4b701ddc749d9696ab8e02296cdc3612d278b243
Richard Cyrus <richard.cyrus@rcyrus.com>
2022-04-10 20:43:08 -0400

    Simplify forwardRef on function components

--- app/components/CommentReplyForm.tsx
+++ app/components/CommentReplyForm.tsx
@@ -9,58 +9,54 @@ interface CommentReplyFormProps {
   productRequestId: number | undefined;
 }
 
-const CommentReplyForm = React.forwardRef(
-  (props: CommentReplyFormProps, ref: ForwardedRef<HTMLFormElement>) => {
-    return (
-      <>
-        <Form
-          ref={ref}
-          className="comment-reply-form"
-          autoComplete="off"
-          method="post"
-        >
-          <input
-            type="hidden"
-            name="replyToCommentId"
-            value={props.replyToCommentId}
-          />
-          <input
-            type="hidden"
-            name="replyingToUsername"
-            value={props.replyingToUsername}
-          />
-          <input
-            type="hidden"
-            name="productId"
-            value={props.productRequestId}
-          />
-          <label htmlFor="add-comment-reply" className="sr-only">
-            Add comment reply
-          </label>
-          <textarea
-            name="content"
-            id="add-comment-reply"
-            cols={30}
-            rows={3}
-            maxLength={250}
-            className="input"
-            placeholder="Type your reply here"
-          />
-          <div className="form-control-group">
-            <button
-              type="submit"
-              className="button button-primary"
-              name="_action"
-              value="comment_reply"
-            >
-              Post Reply
-            </button>
-          </div>
-        </Form>
-      </>
-    );
-  }
-);
-CommentReplyForm.displayName = "CommentReplyForm";
+function CommentReplyForm(
+  props: CommentReplyFormProps,
+  ref: ForwardedRef<HTMLFormElement>
+) {
+  return (
+    <>
+      <Form
+        ref={ref}
+        className="comment-reply-form"
+        autoComplete="off"
+        method="post"
+      >
+        <input
+          type="hidden"
+          name="replyToCommentId"
+          value={props.replyToCommentId}
+        />
+        <input
+          type="hidden"
+          name="replyingToUsername"
+          value={props.replyingToUsername}
+        />
+        <input type="hidden" name="productId" value={props.productRequestId} />
+        <label htmlFor="add-comment-reply" className="sr-only">
+          Add comment reply
+        </label>
+        <textarea
+          name="content"
+          id="add-comment-reply"
+          cols={30}
+          rows={3}
+          maxLength={250}
+          className="input"
+          placeholder="Type your reply here"
+        />
+        <div className="form-control-group">
+          <button
+            type="submit"
+            className="button button-primary"
+            name="_action"
+            value="comment_reply"
+          >
+            Post Reply
+          </button>
+        </div>
+      </Form>
+    </>
+  );
+}
 
-export default CommentReplyForm;
+export default React.forwardRef(CommentReplyForm);
--- app/components/FeedbackComment.tsx
+++ app/components/FeedbackComment.tsx
@@ -4,69 +4,69 @@ import { ForwardedRef, useState } from "react";
 import CommentReply, { CommentReplyProps } from "~/components/CommentReply";
 import CommentReplyForm from "~/components/CommentReplyForm";
 
-const FeedbackComment = React.forwardRef(
-  (props: CommentReplyProps, ref: ForwardedRef<HTMLFormElement>) => {
-    const [isReplyFormOpen, setReplyFormOpen] = useState(false);
+function FeedbackComment(
+  props: CommentReplyProps,
+  ref: ForwardedRef<HTMLFormElement>
+) {
+  const [isReplyFormOpen, setReplyFormOpen] = useState(false);
 
-    const onReplyButtonClick = () => {
-      setReplyFormOpen((currentState) => !currentState);
-    };
+  const onReplyButtonClick = () => {
+    setReplyFormOpen((currentState) => !currentState);
+  };
 
-    const { user } = props;
+  const { user } = props;
 
-    return (
-      <>
-        <div className="feedback-detail-comment" data-comment-id={props.id}>
-          <div className="feedback-detail-comment-heading">
-            <div className="commenter-image">
-              <img src={user.image} alt={`Avatar of ${user.name}`} />
-            </div>
-            <div className="commenter-names">
-              <h4
-                className="h4 commenter-name"
-                aria-label={`Comment from ${user.name}`}
-              >
-                {user.name}
-              </h4>
-              <p className="commenter-username">@{user.username}</p>
-            </div>
-            <button
-              type="button"
-              className="button button-reply"
-              onClick={onReplyButtonClick}
-            >
-              Reply
-            </button>
+  return (
+    <>
+      <div className="feedback-detail-comment" data-comment-id={props.id}>
+        <div className="feedback-detail-comment-heading">
+          <div className="commenter-image">
+            <img src={user.image} alt={`Avatar of ${user.name}`} />
           </div>
-          <p className="comment-text">{props.content}</p>
-          {isReplyFormOpen ? (
-            <CommentReplyForm
-              ref={ref}
-              replyToCommentId={props.id}
-              replyingToUsername={user.username}
-              productRequestId={props.productRequestId}
-            />
-          ) : null}
-          {(props.replies || []).map((reply) => (
-            <CommentReply
-              id={reply.id}
-              content={reply.content}
-              isReply={reply.isReply}
-              replyingTo={reply.replyingTo}
-              parentId={reply.parentId}
-              userId={reply.userId}
-              productRequestId={reply.productRequestId}
-              user={reply.user}
-              replies={reply.replies}
-              ref={ref}
-              key={reply.id}
-            />
-          ))}
+          <div className="commenter-names">
+            <h4
+              className="h4 commenter-name"
+              aria-label={`Comment from ${user.name}`}
+            >
+              {user.name}
+            </h4>
+            <p className="commenter-username">@{user.username}</p>
+          </div>
+          <button
+            type="button"
+            className="button button-reply"
+            onClick={onReplyButtonClick}
+          >
+            Reply
+          </button>
         </div>
-      </>
-    );
-  }
-);
-FeedbackComment.displayName = "FeedbackComment";
+        <p className="comment-text">{props.content}</p>
+        {isReplyFormOpen ? (
+          <CommentReplyForm
+            ref={ref}
+            replyToCommentId={props.id}
+            replyingToUsername={user.username}
+            productRequestId={props.productRequestId}
+          />
+        ) : null}
+        {(props.replies || []).map((reply) => (
+          <CommentReply
+            id={reply.id}
+            content={reply.content}
+            isReply={reply.isReply}
+            replyingTo={reply.replyingTo}
+            parentId={reply.parentId}
+            userId={reply.userId}
+            productRequestId={reply.productRequestId}
+            user={reply.user}
+            replies={reply.replies}
+            ref={ref}
+            key={reply.id}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
 
-export default FeedbackComment;
+export default React.forwardRef(FeedbackComment);
