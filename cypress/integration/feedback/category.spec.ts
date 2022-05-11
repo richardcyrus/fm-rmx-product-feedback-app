9e89acf1181699ebdfa5ad7b6422d254ee9e8230
Richard Cyrus <richard.cyrus@rcyrus.com>
2022-05-11 10:12:40 -0400

    Refactor suggestions category filter test

--- cypress/integration/feedback/category.spec.ts
+++ cypress/integration/feedback/category.spec.ts
@@ -7,53 +7,32 @@ describe("Filter suggestions by category", () => {
 
   /**
    * Considerations:
-   * - Validate correct panel is rendered when there are no suggestions.
-   * - Can/Should fixtures be used to provide consistent test data?
+   * - Can/Should fixtures or other data be used to provide consistent test data?
    */
 
-  it("category === All", () => {
-    cy.findByRole("link", { name: "All" }).click();
-    cy.url().should("include", "/feedback/all");
-    cy.findByRole("link", { name: "All" }).should("have.class", "active");
-    cy.get(".suggestion-category").should("have.length", 6);
-  });
+  const categoryList = [
+    { category: "All", count: 6 },
+    { category: "UI", count: 0 },
+    { category: "UX", count: 0 },
+    { category: "Enhancement", count: 2 },
+    { category: "Bug", count: 1 },
+    { category: "Feature", count: 3 },
+  ];
 
-  it("category === UI", () => {
-    cy.findByRole("link", { name: "UI" }).click();
-    cy.url().should("include", "/feedback/ui");
-    cy.findByRole("link", { name: "UI" }).should("have.class", "active");
-    cy.get(".suggestion-category").should("have.length", 0);
-  });
+  categoryList.forEach((item) => {
+    it(`category === ${item.category}`, () => {
+      cy.findByRole("link", { name: item.category }).click();
+      cy.url().should("include", `/feedback/${item.category.toLowerCase()}`);
+      cy.findByRole("link", { name: item.category }).should(
+        "have.class",
+        "active"
+      );
 
-  it("category === UX", () => {
-    cy.findByRole("link", { name: "UX" }).click();
-    cy.url().should("include", "/feedback/ux");
-    cy.findByRole("link", { name: "UX" }).should("have.class", "active");
-    cy.get(".suggestion-category").should("have.length", 0);
-  });
-
-  it("category === Enhancement", () => {
-    cy.findByRole("link", { name: "Enhancement" }).click();
-    cy.url().should("include", "/feedback/enhancement");
-    cy.findByRole("link", { name: "Enhancement" }).should(
-      "have.class",
-      "active"
-    );
-    cy.get(".suggestion-category").should("have.length", 2);
-  });
-
-  it("category === Bug", () => {
-    cy.findByRole("link", { name: "Bug" }).click();
-    cy.url().should("include", "/feedback/bug");
-    cy.findByRole("link", { name: "Bug" }).should("have.class", "active");
-    cy.get(".suggestion-category").should("have.length", 1);
-  });
-
-  it("category === Feature", () => {
-    cy.findByRole("link", { name: "Feature" }).click();
-    cy.url().should("include", "/feedback/feature");
-    cy.findByRole("link", { name: "Feature" }).should("have.class", "active");
-    cy.get(".suggestion-category").should("have.length", 3);
+      // eslint-disable-next-line chai-friendly/no-unused-expressions
+      item.count === 0
+        ? cy.findByText(/there is no feedback yet/i).should("exist")
+        : cy.get(".suggestion-category").should("have.length", item.count);
+    });
   });
 });
 
