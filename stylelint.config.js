4e6f51414398ae6126ac86266d5a564563bb3da4
Richard Cyrus <richard.cyrus@rcyrus.com>
2022-06-04 15:14:30 -0400

    Update comment rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -7,6 +7,14 @@ module.exports = {
       { ignoreShorthands: ["/flex/", "/grid/"] },
     ],
     "font-family-name-quotes": "always-where-required",
+    "comment-empty-line-before": [
+      "always",
+      {
+        ignoreComments: ["/^#endregion/"],
+        except: ["first-nested"],
+        ignore: ["after-comment", "stylelint-commands"],
+      },
+    ],
     "number-max-precision": 6,
     "order/properties-alphabetical-order": true,
     "selector-class-pattern": null,
