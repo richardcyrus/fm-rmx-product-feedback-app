3b36217c4e1537b295b43d46c2740af19f5be9e5
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-02-27 16:29:35 -0500

    feat: Remove @reach/tabs
    Replace with @radix-ui/react-tabs for React 18 support

--- app/components/RoadmapContentMobile.tsx
+++ app/components/RoadmapContentMobile.tsx
@@ -1,4 +1,4 @@
-import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@reach/tabs";
+import * as RadixTabs from "@radix-ui/react-tabs";
 
 import RoadmapCard from "~/components/RoadmapCard";
 import type { RoadmapContentProps } from "~/components/RoadmapContent";
@@ -9,105 +9,107 @@ function RoadmapContentMobile(props: RoadmapContentProps) {
     <>
       <RoadmapHeader />
       <main role="main">
-        <Tabs>
-          <TabList>
-            <Tab className="tab__planned">
+        <RadixTabs.Root defaultValue="tab__planned">
+          <RadixTabs.List className="tabs-list">
+            <RadixTabs.Trigger
+              className="tab tab__planned"
+              value="tab__planned"
+            >
               <p className="h3 tab-label">
                 Planned ({props.plannedStatusData?.length})
               </p>
-            </Tab>
-            <Tab className="tab__in-progress">
+            </RadixTabs.Trigger>
+            <RadixTabs.Trigger
+              className="tab tab__in-progress"
+              value="tab__in-progress"
+            >
               <p className="h3 tab-label">
                 In-Progress ({props.inProgressStatusData?.length})
               </p>
-            </Tab>
-            <Tab className="tab__live">
+            </RadixTabs.Trigger>
+            <RadixTabs.Trigger className="tab tab__live" value="tab__live">
               <p className="h3 tab-label">
                 Live ({props.liveStatusData?.length})
               </p>
-            </Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <div className="column">
-                {props.plannedStatusData.length > 0 && (
-                  <>
-                    <h2 className="h3 column-heading">
-                      Planned ({props.plannedStatusData.length})
-                    </h2>
-                    <p className="body1 column-description">
-                      Ideas prioritized for research
-                    </p>
-                    {props.plannedStatusData.map((item) => (
-                      <RoadmapCard
-                        id={item.id}
-                        title={item.title}
-                        category={item.category}
-                        upvotes={item.upvotes}
-                        status={item.status}
-                        description={item.description}
-                        comments={item.comments}
-                        key={item.id}
-                      />
-                    ))}
-                  </>
-                )}
-              </div>
-            </TabPanel>
-            <TabPanel>
-              <div className="column">
-                {props.inProgressStatusData.length > 0 && (
-                  <>
-                    <h2 className="h3 column-heading">
-                      In-Progress ({props.inProgressStatusData.length})
-                    </h2>
-                    <p className="body1 column-description">
-                      Currently being developed
-                    </p>
-                    {props.inProgressStatusData.map((item) => (
-                      <RoadmapCard
-                        id={item.id}
-                        title={item.title}
-                        category={item.category}
-                        upvotes={item.upvotes}
-                        status={item.status}
-                        description={item.description}
-                        comments={item.comments}
-                        key={item.id}
-                      />
-                    ))}
-                  </>
-                )}
-              </div>
-            </TabPanel>
-            <TabPanel>
-              <div className="column">
-                {props.liveStatusData.length > 0 && (
-                  <>
-                    <h2 className="h3 column-heading">
-                      Live ({props.liveStatusData.length})
-                    </h2>
-                    <p className="body1 column-description">
-                      Released features
-                    </p>
-                    {props.liveStatusData.map((item) => (
-                      <RoadmapCard
-                        id={item.id}
-                        title={item.title}
-                        category={item.category}
-                        upvotes={item.upvotes}
-                        status={item.status}
-                        description={item.description}
-                        comments={item.comments}
-                        key={item.id}
-                      />
-                    ))}
-                  </>
-                )}
-              </div>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
+            </RadixTabs.Trigger>
+          </RadixTabs.List>
+          <RadixTabs.Content className="tab-panel" value="tab__planned">
+            <div className="column">
+              {props.plannedStatusData.length > 0 && (
+                <>
+                  <h2 className="h3 column-heading">
+                    Planned ({props.plannedStatusData.length})
+                  </h2>
+                  <p className="body1 column-description">
+                    Ideas prioritized for research
+                  </p>
+                  {props.plannedStatusData.map((item) => (
+                    <RoadmapCard
+                      id={item.id}
+                      title={item.title}
+                      category={item.category}
+                      upvotes={item.upvotes}
+                      status={item.status}
+                      description={item.description}
+                      comments={item.comments}
+                      key={item.id}
+                    />
+                  ))}
+                </>
+              )}
+            </div>
+          </RadixTabs.Content>
+          <RadixTabs.Content className="tab-panel" value="tab__in-progress">
+            <div className="column">
+              {props.inProgressStatusData.length > 0 && (
+                <>
+                  <h2 className="h3 column-heading">
+                    In-Progress ({props.inProgressStatusData.length})
+                  </h2>
+                  <p className="body1 column-description">
+                    Currently being developed
+                  </p>
+                  {props.inProgressStatusData.map((item) => (
+                    <RoadmapCard
+                      id={item.id}
+                      title={item.title}
+                      category={item.category}
+                      upvotes={item.upvotes}
+                      status={item.status}
+                      description={item.description}
+                      comments={item.comments}
+                      key={item.id}
+                    />
+                  ))}
+                </>
+              )}
+            </div>
+          </RadixTabs.Content>
+          <RadixTabs.Content className="tab-panel" value="tab__live">
+            <div className="column">
+              {props.liveStatusData.length > 0 && (
+                <>
+                  <h2 className="h3 column-heading">
+                    Live ({props.liveStatusData.length})
+                  </h2>
+                  <p className="body1 column-description">Released features</p>
+                  {props.liveStatusData.map((item) => (
+                    <RoadmapCard
+                      id={item.id}
+                      title={item.title}
+                      category={item.category}
+                      upvotes={item.upvotes}
+                      status={item.status}
+                      description={item.description}
+                      comments={item.comments}
+                      key={item.id}
+                    />
+                  ))}
+                </>
+              )}
+            </div>
+          </RadixTabs.Content>
+        </RadixTabs.Root>
       </main>
     </>
   );
