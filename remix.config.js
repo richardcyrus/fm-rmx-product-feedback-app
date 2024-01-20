80e393df816eaebdccfcc304f4c904ffa6b7802e
Richard Cyrus <richard.cyrus@rcyrus.com>
2024-01-20 16:11:49 -0500

    feat: migrate to v2 route conventions

---
+++ remix.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+export default {
+  ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+};
